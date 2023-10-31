f61f04b209b9895fe6db28922eb0f569381a6373
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-10-31 17:49:44 +0100

    Passing test

---
+++ lib/game.rb
@@ -0,0 +1,4 @@
+class Game
+  def self.turn(board)
+  end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,11 @@
+$:.unshift(File.expand_path('../../lib', __FILE__))
+require 'game'
+
+RSpec.describe Game do
+  describe '.turn' do
+    subject { described_class.turn(board) }
+    let(:board) { nil }
+
+    it { expect(subject).to eq(nil) }
+  end
+end
